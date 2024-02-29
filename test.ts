33fe38509ff1e7ae708dafaff52d1b1b4c145ae0
Andreas Kilde Lien <andreas.kilde-lien@gjensidige.no>
2024-02-29 15:46:53 +0100

    fix of string in test

--- test.ts
+++ test.ts
@@ -1,12 +1,12 @@
 import { assert } from "https://deno.land/std@0.217.0/assert/assert.ts";
 import { randomYeQuote, Quote, YE_QUOTES } from "./mod.ts";
 
-Deno.test("randomYeQuote() returns a valid sushi type", () => {
+Deno.test("randomYeQuote() returns a valid Ye quote", () => {
   const quote: Quote = randomYeQuote();
   assert(YE_QUOTES.includes(quote));
 });
 
-Deno.test("randomYeQuote() returns different sushi types for multiple calls", () => {
+Deno.test("randomYeQuote() returns different Ye quotes for multiple calls", () => {
   const results = new Set<Quote>();
   for (let i = 0; i < 100; i++) {
     results.add(randomYeQuote());
